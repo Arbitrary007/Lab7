eed0c44aff271d257a1c6b021bb0959d9aca357c
Daniel Beck <arbitrary007@yahoo.com>
2017-02-23 18:59:37 -0600

    And now a modified version of the array thingy I made.

--- RandomArray.c
+++ RandomArray.c
@@ -6,4 +6,8 @@ void main(){
   for(i=0; i<1000; i++){
     rand_array[$i]=random();
   }
+  printf("Here's the results of making an array of random integers: \n");
+  for(i=0; i<1000; i++){
+    printf("%d ", rand_array[$i]);
+  }
 }
